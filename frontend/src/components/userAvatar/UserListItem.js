678f62379416bc04f2685ca696ac908805075838
krdev17 <55844638+krdev17@users.noreply.github.com>
2022-07-26 21:18:46 +0530

    Update UserListItem.js

--- frontend/src/components/userAvatar/UserListItem.js
+++ frontend/src/components/userAvatar/UserListItem.js
@@ -2,8 +2,8 @@ import { Avatar } from "@chakra-ui/avatar";
 import { Box, Text } from "@chakra-ui/layout";
 import { ChatState } from "../../Context/ChatProvider";
 
-const UserListItem = ({user, handleFunction }) => {
-  //const { user } = ChatState();
+const UserListItem = ({ handleFunction }) => {
+  const { user } = ChatState();
 
   return (
     <Box
@@ -15,7 +15,7 @@ const UserListItem = ({user, handleFunction }) => {
         color: "white",
       }}
       w="100%"
-      display="flex"
+      d="flex"
       alignItems="center"
       color="black"
       px={3}
